5ed856afef7cc38d9e003e16c94a551f82689cd8
SungSoo Park <whiteibescu@gmail.com>
2020-12-15 00:56:39 +0900

    18

---
+++ LearningPython/18.py
@@ -0,0 +1,10 @@
+﻿import datetime
+
+now = datetime.datetime.now()
+
+if 3 <= now.month <= 5:
+   print("이번 달은 {}월로 봄입니다!".format(now.month))
+
+if 6 <= now.month <= 8:
+    print("이번 달은 {}월로 여름입니다!".format(now.month)
+
