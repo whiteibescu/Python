caac6db4512799b60d9d10ffae3ef0061e942813
SungSoo Park <whiteibescu@gmail.com>
2021-01-28 22:35:32 +0900

    116

---
+++ LearningPython/116.py
@@ -0,0 +1,8 @@
+class Student:
+    def __init__(self, name, korean, math, english, science):
+        self.name = name
+        self.korean = korean
+        self.math = math
+        self.english = english
+        self.science = science
+
