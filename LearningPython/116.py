00db5adb39ad4262047fc41fa43e30fce91e4d2d
SungSoo Park <whiteibescu@gmail.com>
2021-01-29 22:50:53 +0900

    116

--- LearningPython/116.py
+++ LearningPython/116.py
@@ -12,3 +12,22 @@ class Student:
 
     def get_average(self):
         return self.get_sum() / 4
+
+    def to_string(self):
+        return "{}\t{}\t{}".format(\
+            self.name,\
+            self.get_sum(),\
+            self.get_average())
+
+students = [
+    Student("운인성", 87, 98, 88, 95),
+    Student("연하진", 92, 98, 96, 98),
+    Student("구지연", 76, 96, 94, 90),
+    Student("나선주", 98, 92, 96, 92),
+    Student("윤아린", 95, 98, 98, 98),
+    Student("윤명월", 64, 88, 92, 92)
+]
+
+print("이름", "총점", "평균", sep="\t")
+for student in students:
+    print(student.to_string())
