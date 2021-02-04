d8833914bec2fa8dad34ec35f16416df4d6e15e8
SungSoo Park <whiteibescu@gmail.com>
2021-02-04 21:38:00 +0900

    126

---
+++ LearningPython/126.py
@@ -0,0 +1,15 @@
+class Parent:
+    def __init__(self):
+        self.value = "테스트"
+        print("Parent 클래스의 __init()__ 메소드가 호출되었습니다.")
+    def test(self):
+        print("Parent 클래스의 test() 메소드입니다.")
+
+class Child(Parent):
+    def __init__(self):
+        Parent.__init__(self)
+        print("Child 클래스의 __init()__ 메소드가 호출되었습니다.")
+
+child = Child()
+child.test()
+print(child.value)
